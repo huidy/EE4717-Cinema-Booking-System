5521ce66b6d19078828e0f095bd23e2d758819f3
Brendan Ho <brendanhwz24@gmail.com>
2021-11-01 23:54:40 +0800

    Commit to include submit button on showtimings.php

--- seatselection.php
+++ seatselection.php
@@ -1,6 +1,8 @@
 <?php
 include "scripts/php/setupdb.php";
 include "scripts/php/fill_showtiming_table.php";
+//Retrieving the superglobal variable from showtiming.php
+$time_choice = $_GET['timeChoice'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
